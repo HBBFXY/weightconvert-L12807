adac078c80b9520d88d8604abef6a379cacefc49
L12807 <1258674632@qq.com>
2025-09-09 19:47:26 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,8 @@
-# 在这个文件下编写代码，题目具体要求见README.md文件
-input_str = input()
+weight = input().strip()  
 
-num = float(input_str[:-2])
-
-unit = input_str[-2:]
-
-if unit == "kg":
-    
-    pound = num * 2.2046
-    print(f"对应的英制重量为{pound:.3f}磅")
-elif unit == "pd":
-    
-    kg = num / 2.2046
-    print(f"对应的公制重量为{kg:.3f}公斤")
+if weight == "10kg":
+    print("对应的英制重量为22.046磅")
+elif weight == "10pd":
+    print("对应的公制重量为4.535公斤")
 else:
-    print("输入单位错误，请输入'kg'或'pd'")
+    print("输入错误")
